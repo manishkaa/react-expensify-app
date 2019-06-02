dbd568375c19ff0596270a53ad8f1d547203bd32
manishkaa <35163295+manishkaa@users.noreply.github.com>
2019-06-02 19:27:53 +0530

    added

--- src/app.js
+++ src/app.js
@@ -15,7 +15,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-
 const jsx = (
   <Provider store={store}>
   <AppRouter />
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 //entry point (source file)
 const path = require('path');//built in node function
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports =(env) =>{
   const isProduction = env === 'production';
- 
+  const CSSExtract = new ExtractTextPlugin('style.css');
   return {
     entry:"./src/app.js",
     output: {
@@ -16,14 +17,18 @@ module.exports =(env) =>{
         exclude:/node_modules/
       }, {
         test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: CSSExtract.extract({
+          use: [
+            'css-loader',
+            'sass-loader'
+          ]
+        })
       }]
     },
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer:{
       contentBase:path.join(__dirname,'public'),
       historyApiFallback: true
